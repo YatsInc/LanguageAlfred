8271c98b2d3c765deaa7046d1e4814a30daf09cc
DennY <45600211+DennYats@users.noreply.github.com>
2022-02-19 20:58:34 +0200

    Voice recognition algorithm separated from Program.cs - now it's only the entry point.

--- L-Alfred/Program.cs
+++ L-Alfred/Program.cs
@@ -1,68 +1,11 @@
-﻿using Microsoft.CognitiveServices.Speech;
-using Microsoft.CognitiveServices.Speech.Audio;
-using Microsoft.Extensions.Configuration;
+﻿using L_Alfred.VoiceRecognition;
 using System.Text;
 using static L_Alfred.Languages;
-
 
 Console.InputEncoding = Encoding.UTF8;
 Console.OutputEncoding = Encoding.UTF8;
 
 ShowInstalledLanguages();
 
-var config = new ConfigurationBuilder()
-            .AddUserSecrets<Program>()
-            .Build();
-
-var subsribtionKey = config["SubsribtionKey"];
-var apiRegion = config["Region"];
-
-var speechConfig = SpeechConfig.FromSubscription(subsribtionKey, apiRegion);
-
-await RecognizeCommand();
-
-async Task RecognizeCommand()
-{
-    using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
-    using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
-
-    var stopRecognition = new TaskCompletionSource<int>();
-
-    recognizer.Recognizing += (s, e) =>
-    {
-        ChangeLanguage(e.Result.Text);
-    };
-
-    recognizer.Recognized += (s, e) =>
-    {
-        if (e.Result.Reason == ResultReason.NoMatch)
-        {
-            Console.WriteLine($"NOMATCH: Speech could not be recognized.");
-        }
-    };
-
-    recognizer.Canceled += (s, e) =>
-    {
-        Console.WriteLine($"CANCELED: Reason={e.Reason}");
-
-        if (e.Reason == CancellationReason.Error)
-        {
-            Console.WriteLine($"CANCELED: ErrorCode={e.ErrorCode}");
-            Console.WriteLine($"CANCELED: ErrorDetails={e.ErrorDetails}");
-            Console.WriteLine($"CANCELED: Did you update the speech key and location/region info?");
-        }
-
-        stopRecognition.TrySetResult(0);
-    };
-
-    recognizer.SessionStopped += (s, e) =>
-    {
-        Console.WriteLine("\n    Session stopped event.");
-        stopRecognition.TrySetResult(0);
-    };
-
-    Console.WriteLine("Speak into your microphone.");
-    await recognizer.StartContinuousRecognitionAsync();
-
-    Task.WaitAny(new[] { stopRecognition.Task });
-}
+var recognizer = new AzureSpeechToText();
+await recognizer.RecognizeCommandAsync();
---
+++ L-Alfred/VoiceRecognition/AzureSpeechToText.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.Extensions.Configuration;
+using static L_Alfred.Languages;
+
+namespace L_Alfred.VoiceRecognition;
+
+public class AzureSpeechToText
+{
+    private IConfigurationRoot Config { get; set; }
+
+    private string SubsribtionKey { get; set; }
+
+    private string ApiRegion { get; set; }
+
+    private SpeechConfig SpeechConfig { get; set; }
+
+    public AzureSpeechToText()
+    {
+        Config = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+
+        SubsribtionKey = Config["SubsribtionKey"];
+        ApiRegion = Config["Region"];
+
+        SpeechConfig = SpeechConfig.FromSubscription(SubsribtionKey, ApiRegion);
+    }
+
+    public async Task RecognizeCommandAsync()
+    {
+        using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+        using var recognizer = new SpeechRecognizer(SpeechConfig, audioConfig);
+
+        var stopRecognition = new TaskCompletionSource<int>();
+
+        recognizer.Recognizing += (s, e) =>
+        {
+            ChangeLanguage(e.Result.Text);
+        };
+
+        recognizer.Recognized += (s, e) =>
+        {
+            if (e.Result.Reason == ResultReason.NoMatch)
+            {
+                Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+            }
+        };
+
+        recognizer.Canceled += (s, e) =>
+        {
+            Console.WriteLine($"CANCELED: Reason={e.Reason}");
+
+            if (e.Reason == CancellationReason.Error)
+            {
+                Console.WriteLine($"CANCELED: ErrorCode={e.ErrorCode}");
+                Console.WriteLine($"CANCELED: ErrorDetails={e.ErrorDetails}");
+                Console.WriteLine($"CANCELED: Did you update the speech key and location/region info?");
+            }
+
+            stopRecognition.TrySetResult(0);
+        };
+
+        recognizer.SessionStopped += (s, e) =>
+        {
+            Console.WriteLine("\n    Session stopped event.");
+            stopRecognition.TrySetResult(0);
+        };
+
+        Console.WriteLine("Speak into your microphone.");
+        await recognizer.StartContinuousRecognitionAsync();
+
+        Task.WaitAny(new[] { stopRecognition.Task });
+    }
+}
