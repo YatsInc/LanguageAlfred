6dc27e6140b891229132375eecc882bfc0ed7b28
DennY <45600211+DennYats@users.noreply.github.com>
2022-02-19 21:06:26 +0200

    minor fix

--- L-Alfred/Constants.cs
+++ L-Alfred/Constants.cs
@@ -2,7 +2,7 @@
 
 public static class Constants
 {
-    public readonly static IntPtr window = (IntPtr)0xffff;
+    public static readonly IntPtr window = (IntPtr)0xffff;
     public const int WM_INPUTLANGCHANGEREQUEST = 0x0050;
     public const int WM_INPUTLANGCHANGE = 0x0051;
 }
